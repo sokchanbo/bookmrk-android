131e8b25a3fd4b4c4afba00d74b804723cdc3246
Sokchanbo LY <ly.sokchanbo@abahubdiglybo.local>
2024-01-05 16:17:12 +0700

    remove firebase crashlytics

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -21,8 +21,6 @@ tasks.withType<KotlinCompile>().configureEach {
 dependencies {
     compileOnly(libs.android.gradlePlugin)
     compileOnly(libs.android.tools.common)
-    compileOnly(libs.firebase.crashlytics.gradlePlugin)
-    compileOnly(libs.firebase.performance.gradlePlugin)
     compileOnly(libs.kotlin.gradlePlugin)
     compileOnly(libs.ksp.gradlePlugin)
 }
